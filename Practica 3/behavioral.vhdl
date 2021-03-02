94b459834ad4330f42b73ee2bf97e0dba6007461
ArathHernandez <47037125+ArathHernandez@users.noreply.github.com>
2021-03-02 15:18:27 -0600

    logic

---
+++ Practica 3/behavioral.vhdl
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity source02 is
+    Port ( P1 : in STD_LOGIC;
+           P2 : in STD_LOGIC;
+           P3 : in STD_LOGIC;
+           A : out STD_LOGIC;
+           B : out STD_LOGIC;
+           C : out STD_LOGIC;
+           D : out STD_LOGIC);
+end source02;
+
+architecture Behavioral of source02 is
+
+begin
+A <= (P1 AND P2 AND P3);
+B <= (NOT P1 OR NOT P2 OR NOT P3);
+C <= (P2 AND P3) OR (P1 AND P3) OR (P1 AND P2);
+D <= (NOT P1 AND NOT P2) OR (NOT P1 AND NOT P3) OR (NOT P2 AND NOT P3);
+end Behavioral;
